e6651fc26bcdb68e9aab06f6d06b2a2ac835c800
Arthur Fernandes <arthurfernandes893@gmail.com>
2025-01-21 14:08:03 -0300

    Gerando arquivo CSV com os resultados da consulta

--- Issues/issue-metrics.js
+++ Issues/issue-metrics.js
@@ -1,8 +1,10 @@
 import functions from './issueFunctions.js';
+import fs from 'fs';
 let labels = ['incidente','incidente-critico', 'vulnerabilidade','vulnerabilidade-critica'];
 
 async function listIssues() {
     try {
+        let fileData = 'title;labels;assignees;daysOpen';
         for (const label of labels) {
 
             /*
@@ -28,11 +30,32 @@ async function listIssues() {
             console.log('-'.repeat(50));
 
             if (issues.length > 0) {
+                issues.forEach(issue => {
+                    fileData += `\n${issue.title};${issue.labels};${issue.assignees};${issue.DaysOpen}`;
+                });
+
                 console.table(issues);
             } else {
                 console.log(`No issues found for label: ${label} + PRD`);
             }  
         }
+       
+        const fileName = `Issues/results/issues.csv`;
+        console.log(`\nGerando Arquivo ${fileName}...`);
+
+        fs.writeFile(fileName, fileData, { encoding: 'utf-8' }, (err) => {
+            if (err) {
+                if(err.code === 'EBUSY'){
+                    console.error(`\n - Arquivo ${fileName} em uso. Feche o arquivo e tente novamente.`);
+                }
+                else{
+                    console.error(`\nErro gerando Arquivo CSV: ${err}`);
+                }
+            } else {
+                console.log(` - Arquivo ${fileName} gerado com sucesso.`);
+            }
+        });
+
     } catch (error) {
         console.error(`Error ${error.status} while fetching issues:`, error);
     }
